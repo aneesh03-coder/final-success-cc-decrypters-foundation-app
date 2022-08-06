9dced05191eec0a78c4645e1b18a4a270f0a4533
unknown <aneeshshillong3@gmail.com>
2022-08-07 01:24:44 +0530

    Fixed Vercel Errors again1

--- pages/api/create-stripe-session.js
+++ pages/api/create-stripe-session.js
@@ -1,31 +1,31 @@
-const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 async function CreateStripeSession(req, res) {
-    const { donation } = req.body
-    const redirectURL = "http://localhost:3000"
-    const transformedData = {
-        price_data: {
-            currency: "usd",
-            product_data: {
-                name: donation.name,
-            },
-            unit_amount: donation.price * 100,
-        },
-        quantity: 1,
-    }
+  const { donation } = req.body;
+  const redirectURL = `${process.env.NEXT_PUBLIC_BASE_URL}`;
+  const transformedData = {
+    price_data: {
+      currency: "usd",
+      product_data: {
+        name: donation.name,
+      },
+      unit_amount: donation.price * 100,
+    },
+    quantity: 1,
+  };
 
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ["card"],
-        line_items: [transformedData],
-        mode: "payment",
-        success_url: redirectURL + "/Success",
-        cancel_url: redirectURL + "/Cancel",
-        metadata: {
-            name: donation.name,
-        },
-    })
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    line_items: [transformedData],
+    mode: "payment",
+    success_url: redirectURL + "/Success",
+    cancel_url: redirectURL + "/Cancel",
+    metadata: {
+      name: donation.name,
+    },
+  });
 
-    res.status(200).json({id: session.id})
+  res.status(200).json({ id: session.id });
 }
 
-export default CreateStripeSession
+export default CreateStripeSession;
