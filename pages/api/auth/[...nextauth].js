e89063e38d8e89da62842f9703d7f639206df733
unknown <aneeshshillong3@gmail.com>
2022-08-09 00:45:49 +0530

    Deployment Correction for CORS policies

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -7,6 +7,8 @@ export default NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
+
     // ...add more providers here
   ],
-})
+  secret: process.env.NEXT_PUBLIC_SECRET,
+});
